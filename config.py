b4286e3b806874af56f2f94aa5dcdb669640d421
RedrickLaw <53625346+RedrickLaw@users.noreply.github.com>
2024-10-14 19:01:24 +0300

    Add server

--- config.py
+++
@@ -1,2 +0,0 @@
-token = '7846844504:AAHNWi2W3P-GDy7oiod7CbBvhyHPRDQlh4E'  # Токен бота
-database_name = "D:\\SQLite DB\\tgzeroone_bot.db"  # Файл с базой данных
--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-import config
-from database import SQLighter
-import aiohttp
-import asyncio
-import logging
-import json
+import asyncio, logging, json, server, os
+from dotenv import find_dotenv, load_dotenv
 from aiogram import Bot, Dispatcher, types, F
-from aiogram.types import KeyboardButton, WebAppInfo, InlineKeyboardButton, InlineKeyboardMarkup
 from aiogram.utils.keyboard import ReplyKeyboardBuilder
 from aiogram.enums.content_type import ContentType
 from aiogram.filters import Command
@@ -14,23 +9,20 @@ from aiogram.enums.parse_mode import ParseMode
 # Логирование
 logging.basicConfig(level=logging.INFO)
 
-# Обработка базы
-db = SQLighter(config.database_name)
-print(db.select_single("test1")[1])
-if db.select_single("test1")[1] != "test1":
-   db.insert_new("'test1'", "'redricklaw'", 220)
-   
+# Получение переменных окружения 
+load_dotenv(find_dotenv('config.env'))
+TOKEN = os.environ.get("token")
+
 # Обработка бота
-bot = Bot(token=config.token)
+bot = Bot(token=TOKEN)
 dp = Dispatcher()
 
 @dp.message(Command("start"))
 async def start(message: types.Message):
-    webAppInfo = types.WebAppInfo(url="https://redricklaw.github.io/TgZeroOne_bot/")
-    builder = ReplyKeyboardBuilder()
-    builder.add(types.KeyboardButton(text='Play!', web_app=webAppInfo))
-    
-    await message.answer(text='Привет!', reply_markup=builder.as_markup())
+   webAppInfo = types.WebAppInfo(url="https://redricklaw.github.io/TgZeroOne_bot/")
+   builder = ReplyKeyboardBuilder()
+   builder.add(types.KeyboardButton(text='Play!', web_app=webAppInfo))
+   await message.answer(text='Привет!', reply_markup=builder.as_markup())
 
 @dp.message(F.content_type == ContentType.WEB_APP_DATA)
 async def parse_data(message: types.Message):
@@ -38,15 +30,9 @@ async def parse_data(message: types.Message):
     await message.answer(f'<b>{data["title"]}</b>\n\n<code>{data["desc"]}</code>\n\n{data["text"]}', parse_mode=ParseMode.HTML)
 
 async def main():
-   async with aiohttp.ClientSession() as session:
-      async with session.get('http://python.org') as response:
-         print("Status:", response.status)
-         print("Content-type:", response.headers['content-type'])
-         html = await response.text()
-         print("Body:", html[:15], "...")
    await bot.delete_webhook(drop_pending_updates=True)
    await dp.start_polling(bot)
+   await server.start_server()
     
-db.close()
 if __name__ == "__main__":
    asyncio.run(main())
